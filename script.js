088ba3527518aedfa9eaee7398c468f0c311a5e1
swaragsharma <52152473+swaragsharma@users.noreply.github.com>
2019-06-25 11:00:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var x = document.createElement("p");
+var node = document.createTextNode("Currency Convertor");
+x.appendChild(node);
+document.body.appendChild(x);
+x.classList.add("weight");
+x.setAttribute("style","text-shadow:2px 2px 10px red");
+
